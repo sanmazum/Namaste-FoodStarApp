8536058e5dd6c2cfa375aab5516cefb44151c7c9
Sandeepan Mazumdar <rony99sandy@outlook.com>
2023-12-31 02:19:43 +0530

    episode 2 igniting the app

--- App.js
+++ App.js
@@ -1,18 +1,6 @@
-/**
- * Create a nested tag like below using React
- *
- * <div id="parent">
- *      <div id="child"></div>
- *      <h1>hi , I am an h1 tag </h1>
- *      <h2> hi i am h2 tag </h2>  --> creating  nested siblings
- *    </div>
- * <div id ="child 2">
- * <h1>hi , I am an h1 tag </h1>
- *      <h2> hi i am h2 tag </h2>
- * </div>
- *
- * ReactElements(Object) => HTML(Browswer Understands)
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 
 const parent = React.createElement(
   "div",
@@ -21,7 +9,7 @@ const parent = React.createElement(
     "div",
     { id: "child" },
     [
-      React.createElement("h1", {}, "Hi,I am a H1 tag"),
+      React.createElement("h1", {}, "This is a Namaste react"),
       React.createElement("h1", {}, "Hi,I am a H2 tag"),
     ] // write the children in an array
   ),
@@ -29,12 +17,6 @@ const parent = React.createElement(
     React.createElement("h1", {}, "Hi,I am a H1 tag"),
     React.createElement("h1", {}, "Hi,I am a H2 tag"),
   ])
-);
-
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React!"
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- Readme.md
+++ Readme.md
@@ -1,1 +1,22 @@
 # This is Namaste React- Where I have learnt the React from absolute basics and learnt all the concepts.
+
+#Parcel
+
+-Dev Build
+-local server
+-HMR-> Hot module Replacement (automatic render without refreshing)
+-parcel uses a file watching Algorithm - written in C++
+faster builds -> because of Caching
+Image Optimization 
+-Minification of our file
+-Bundler
+-Compress the file
+-consistent hashing
+-code splitting
+-diffrential bundling - to support older browsers
+-Diagnostics
+-good error handling
+-HTTPs
+-Zero config tool
+-tree shaking - remove unused code for you
+-Diffrent dev and prod bundles
